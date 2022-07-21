1132f18e9f3dfb28ba398b991093397080856838
debora <debora.r.serra@gmail.com>
2022-07-21 16:19:11 -0300

    trying again

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,7 @@
 require('dotenv/config');
 const express = require('express');
 const rescue = require('express-rescue');
+
 const UserController = require('../controllers/user');
 const errorMiddleware = require('../middlewares/error');
 
